9b0c203d26b36ce8bbd7d212362a1ce9e52493b2
Il tuo nome <la_tua_email@example.com>
2024-05-15 16:36:46 +0200

    added loop to print as many times as array lenght

--- js/script.js
+++ js/script.js
@@ -1,49 +1,28 @@
-// const frutta = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
-// frutta.push("pesca");
-// let trovato = false
-
-//     for (let index = 0; index < frutta.length; index++) 
-//         if (frutta[index] === "cocomero") {
-//            trovato = true;
-
-       
-        
-//     }
-
-//     if(trovato) {
-//      console.log("Trovato! Devo solo preparare il concktail!")
-//     } else {
-//         console.log("Oh no, devo uscire a comprare il cocomero!")
-//     }
-
-
-// Consegna:
-// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
-
-//     HC
-
-//     //  * Creare lista della spesa
-//         * Creare un loop che scorra gli elementi della lista e li aggiunga in pagina, finchè il numero di elementi nella pagina non sia uguale a quelli della lista della spesa.
-
-
-//         <section class="spesa">
-            
-//             <ul class="lista-spesa">
-
-//             </ul>
-
-//         </section>
-
-
-
 const listaDellaSpesa = ['Pollo', 'Pane', 'Mele', 'Olio', 'Latte', 'Pomodori', 'Riso', 'Lattuga', 'Formaggio', 'Pasta', 'Uova', 'Banane', 'Cereali', 'Yogurt'];
 
 
 
-let ulSelector = document.querySelector("ul");
 
-const createLi = document.createElement('li');
 
-createLi.innerHTML = ("blabla");
+fullList = listaDellaSpesa.length;
 
-ulSelector.appendChild(createLi);
+
+let sum = 0;
+
+let i = 0
+
+while (sum < fullList) {
+
+    sum = sum += 1;
+
+    let ulSelector = document.querySelector("ol");
+
+    const createLi = document.createElement('li');
+
+    createLi.innerHTML = ("blabla");
+
+    ulSelector.appendChild(createLi);
+
+
+}
+
