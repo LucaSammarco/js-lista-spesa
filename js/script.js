3e40c0a9336c7eaff4b89ef83add36edabeaebca
Il tuo nome <la_tua_email@example.com>
2024-05-15 16:50:31 +0200

    put the counter increaser at the end of the loop and now it works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Consigli:
+// Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+
 const listaDellaSpesa = ['Pollo', 'Pane', 'Mele', 'Olio', 'Latte', 'Pomodori', 'Riso', 'Lattuga', 'Formaggio', 'Pasta', 'Uova', 'Banane', 'Cereali', 'Yogurt'];
 
 
@@ -11,9 +18,9 @@ let sum = 0;
 
 let i = 0
 
-while (sum < fullList) {
+while (sum < (fullList)) {
 
-    sum = sum += 1;
+
 
     let ulSelector = document.querySelector("ol");
 
@@ -23,6 +30,7 @@ while (sum < fullList) {
 
     ulSelector.appendChild(createLi);
 
+    sum = sum += 1;
 
 }
 
